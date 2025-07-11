cffa7d8dc48a6a37cf44b88e6118c890bbe4162b
Chichi Otti <co2323@nyu.edu>
2025-07-11 01:26:46 +0000

    Add webhook to aut-update from Github

--- appmain.py
+++ appmain.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+import git 
+
+from flask import Flask, render_template, url_for, flash, redirect, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")  # do you have home.html in templates?
+
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/chinweotapp/mysite/apprepo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == "__main__":
     app.run()
